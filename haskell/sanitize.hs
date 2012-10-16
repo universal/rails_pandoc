4aa8a10cb28939968eac64186ab8e80a901a675b
universa1 / jhedtrich <jhedtrich@gmail.com>
2012-10-16 08:19:21 +0200

    updated sanitize.hs to bjp's version

--- haskell/sanitize.hs
+++ haskell/sanitize.hs
@@ -1,25 +1,18 @@
--- compiled with: ghc 7.2 / sanitize-xss 0.31
-import Data.Text.IO as D
-import Text.HTML.SanitizeXSS
-import System.IO as S
-import Data.Text (Text)
+-- This binary sanitizes html-fragments passed in to avoid
+-- cross-site scripting (XSS) attacks.
+--
+-- The string to be sanitized is read from stdin and the result is written
+-- to stdout.
+module Main where
+
+import Data.Text.IO          as D (hGetContents, hPutStr)
+import Text.HTML.SanitizeXSS      (sanitizeBalance)
+import System.IO             as S (hClose, stdin, stdout)
 
 main :: IO ()
 main = do
-       inpStr <- D.hGetContents S.stdin
-       let result = sanitizeBalance inpStr
-       D.hPutStr S.stdout result
-       S.hClose S.stdin
-       S.hClose S.stdout
-
--- compiled with: ghc 6.2 / 7.0 / older sanitize-xss package
--- import System.IO
--- import Text.HTML.SanitizeXSS
--- 
--- main :: IO ()
--- main = do 
---        inpStr <- hGetContents stdin
---        let result = sanitizeBalance inpStr
---        hPutStr stdout result
---        hClose stdin
---        hClose stdout
+  -- sanitize
+  D.hGetContents S.stdin >>= D.hPutStr S.stdout . sanitizeBalance
+  -- clean up
+  S.hClose S.stdin
+  S.hClose S.stdout
